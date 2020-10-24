94ce416bd46d83b61d3a05a61a1094f8b9cf6ad2
max <max.smith@duke.edu>
2020-10-24 11:12:58 -0400

    Processed data into intermediate train/test sets

--- src/d01_data/data_load.py
+++ src/d01_data/data_load.py
@@ -29,7 +29,30 @@ with open(DATA_PATH + TEST_FILENAME, "r") as test_file:
         digit_num = min(empty_lines // block_size, 9)
         line_number += 1
 
-        cur_write_file = f"{INTERMEDIATE_PATH}0{digit_num}.txt"
+        cur_write_file = f"{INTERMEDIATE_PATH}test_digits/test_0{digit_num}.txt"
+        # print(f"empty_lines: {empty_lines}\tcur_write_file: {cur_write_file}")
+
+        # Check if digit is an empty line, this keeps track of digit separation 
+        if len(line.strip()) == 0:
+            empty_lines += 1
+
+        # do writing to file
+        f = open(cur_write_file, "a")
+        f.write(line)
+        f.close()
+
+empty_lines = 0
+digit_num = 0
+block_size = TRAIN_BLOCK_SIZE
+line_number = -1
+
+with open(DATA_PATH + TRAIN_FILENAME, "r") as test_file:
+    for line in test_file:
+
+        digit_num = min(empty_lines // block_size, 9)
+        line_number += 1
+
+        cur_write_file = f"{INTERMEDIATE_PATH}train_digits/train_0{digit_num}.txt"
         # print(f"empty_lines: {empty_lines}\tcur_write_file: {cur_write_file}")
 
         # Check if digit is an empty line, this keeps track of digit separation 
