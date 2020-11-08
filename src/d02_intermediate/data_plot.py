f2dc91d764ef665b48f4735f32f3e2fb9634a3c2
max <max.smith@duke.edu>
2020-11-08 18:56:32 -0500

    Basic MFCC Plots

--- src/d02_intermediate/data_plot.py
+++ src/d02_intermediate/data_plot.py
@@ -1,17 +1,36 @@
-import pandas as pd
-import numpy as np
+# importing dependencies
+import pandas as pd # data frame
+import numpy as np # matrix math
+import os # interation with the OS
+from sklearn.utils import shuffle # shuffling of data
+from matplotlib import cm
+import matplotlib.pyplot as plt # to view graphs
 
 # Relative filepath names
-INTERMEDIATE_PATH = 'data/02_intermediate/'
-FILE_PATH = 'single_person/test_00_female.txt'
+INTERMEDIATE_PATH = 'data/02_intermediate/single_person/test_00_female_'
+file_00 = f"{INTERMEDIATE_PATH}00.txt"
+file_01 = f"{INTERMEDIATE_PATH}01.txt"
 
-# Try to parse data with file opening
-empty_lines = 0
-digit_num = 0
-filename = f"{INTERMEDIATE_PATH}{FILE_PATH}"
+print(f"file_00: {file_00}")
+df_00 = pd.read_csv(file_00, delimiter=' ', header=None)
 
-# Read file
-df = pd.read_csv(filename, delimiter=' ', header=None)
-print(df)
 
-# TODO - Print MFCC Coefficients
+
+# Plot MFCC Coefficients
+fig, ax = plt.subplots()
+mfcc_data= np.swapaxes(df_00, 0 ,1)
+cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
+ax.set_title('MFCC (00)')
+plt.savefig(f"{INTERMEDIATE_PATH}00.png")
+plt.show()
+
+
+print(f"file_01: {file_01}")
+df_01 = pd.read_csv(file_01, delimiter=' ', header=None)
+
+fig, ax = plt.subplots()
+mfcc_data= np.swapaxes(df_01, 0 ,1)
+cax = ax.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
+ax.set_title('MFCC (01)')
+plt.savefig(f"{INTERMEDIATE_PATH}01.png")
+plt.show()
