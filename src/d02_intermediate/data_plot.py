e31ac426e80ba2398dcd77139457a35465440b29
max <max.smith@duke.edu>
2020-11-18 11:08:36 -0500

    Updated noteboook with mfcc plots by digit

--- src/d02_intermediate/data_plot.py
+++ src/d02_intermediate/data_plot.py
@@ -8,6 +8,7 @@ from matplotlib import cm
 from matplotlib import colors as mcolors
 from matplotlib import colorbar
 import matplotlib.pyplot as plt # to view graphs
+from itertools import combinations # all combinations for mfcc plot
 
 def cc(arg):
     return mcolors.to_rgba(arg, alpha=0.6)
@@ -66,6 +67,18 @@ def plot_mfcc_linear(df, coeffs, filename, save):
         plt.savefig(filename)
     plt.show()
 
+def plot_mfcc_i_j(df, digit, i, j):
+    # Plot MFCC Coefficient i vs coefficient j
+    fig, ax = plt.subplots()
+    coeff_i = df[i]
+    coeff_j = df[j]
+    marker_size = 0.5
+    plt.scatter(x=df[i], y=df[j], s=marker_size, marker=random_marker())        
+    ax.set_title(f"MFCC {i} vs {j} for digit {digit}")
+    plt.ylabel(f"MFCC {j}")
+    plt.xlabel(f"MFCC {i}")
+    plt.show()
+
 # Relative filepath names
 INTERMEDIATE_PATH = 'data/02_intermediate/single_person/test_00_female_'
 file_00 = f"{INTERMEDIATE_PATH}00.txt"
@@ -76,10 +89,74 @@ df_00 = pd.read_csv(file_00, delimiter=' ', header=None)
 df_01 = pd.read_csv(file_01, delimiter=' ', header=None)
 
 # Construct mfcc colorbar from dataframes
-plot_mfcc_colorbar(df=df_00, filename=f"{INTERMEDIATE_PATH}colorbar_00.png")
-plot_mfcc_colorbar(df=df_01, filename=f"{INTERMEDIATE_PATH}colorbar_01.png")
+# plot_mfcc_colorbar(df=df_00, filename=f"{INTERMEDIATE_PATH}colorbar_00.png")
+# plot_mfcc_colorbar(df=df_01, filename=f"{INTERMEDIATE_PATH}colorbar_01.png")
 
 # Plot MFCC Coefficients as function of analysis window
 COEFFS = 4
-plot_mfcc_linear(df=df_00, coeffs=COEFFS, filename=f"{INTERMEDIATE_PATH}linear_00.png", save=True)
-plot_mfcc_linear(df=df_01, coeffs=COEFFS, filename=f"{INTERMEDIATE_PATH}linear_01.png", save=True)
+# plot_mfcc_linear(df=df_00, coeffs=COEFFS, filename=f"{INTERMEDIATE_PATH}linear_00.png", save=True)
+# plot_mfcc_linear(df=df_01, coeffs=COEFFS, filename=f"{INTERMEDIATE_PATH}linear_01.png", save=True)
+
+# Generate all two dimensional combinatoins of MFCC coefficients
+comb = combinations(range(0, 13), 2)
+DIGIT = 0
+
+total_plots = 20
+
+# Relative filepath names
+INTERMEDIATE_PATH = 'data/02_intermediate/train_digits/train_'
+test_file = f"{INTERMEDIATE_PATH}00.txt"
+
+# Load file data into dataframes
+df_test = pd.read_csv(test_file, delimiter=' ', header=None)
+
+# Plot MFCC Coefficients against eachother for i, i+1
+# for i in range(12):
+#     plot_mfcc_i_j(df=df_test, digit=DIGIT, i=i, j=i+1)
+
+# Plot MFCC Coefficients against eachother for ALL pairwise combos
+# for combo in list(comb):
+#     if (total_plots <= 0):
+#         break
+#     i = combo[0]
+#     j = combo[1]
+#     plot_mfcc_i_j(df=df_test, digit=DIGIT, i=i, j=j)
+#     total_plots -= 1;
+
+# Relative path of training data
+TRAIN_PATH = 'data/02_intermediate/train_digits/train_0'
+
+def subplot_mfccs(digits, max_mfcc, fig_width, fig_height):
+    # Create subplots of multiple digits
+    dataframes = []
+    for digit in digits:
+        filename = f"{TRAIN_PATH}{digit}.txt"
+        df = pd.read_csv(filename, delimiter=' ', header=None)
+        dataframes.append(df)
+
+    for row in range(max_mfcc):
+        fig = plt.figure(figsize=(fig_width, fig_height))
+        for col in range(len(digits)):
+            # Fetch digit and dataframe
+            digit = digits[col]
+            df = dataframes[col]
+            i, j = row, row+1
+
+            # Assign plot subplots and axes
+            marker_size = 0.5
+            ax = fig.add_subplot(1, len(digits), col+1)
+            ax.set_title(f"MFCC {i} vs {j} for digit {digit}")
+            
+            plt.scatter(x=df[i], y=df[j], s=marker_size, marker=random_marker(), color=random_color())        
+            ax.set_title(f"Digit {digit}")
+            plt.xlabel(f"MFCC {i}")
+
+            if (col == 0):
+                plt.ylabel(f"MFCC {j}")
+        
+        plt.show()
+
+digits = [0, 4, 9]
+max_mfcc = 4 #12
+fig_width, fig_height = 14, 4
+subplot_mfccs(digits=digits, max_mfcc=max_mfcc, fig_width=fig_width, fig_height=fig_height)
