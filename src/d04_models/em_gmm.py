49343e9a05e27d6a3ffbb5d388a69a53a6088280
max <max.smith@duke.edu>
2020-11-20 11:25:24 -0500

    Updated log space, 90% classification accuracy

---
+++ src/d04_models/em_gmm.py
@@ -0,0 +1,9 @@
+from sklearn.cluster import KMeans
+from scipy.stats import multivariate_normal
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+import random
+import plot_utils as pu
+import sys
+from datetime import datetime as dt
